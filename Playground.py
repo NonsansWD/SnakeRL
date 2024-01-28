d1afd24aabeccd3a5abdece50518908c9be3d8ad
malina <malina@rhrk.uni-kl.de>
2024-01-28 12:46:48 +0100

    improve constants

--- Playground.py
+++ Playground.py
@@ -1,12 +1,20 @@
 import pygame
 import sys
+import enum
+
+
+class Color(enum.Enum):
+    WHITE = (255, 255, 255)
+    GREY = (200, 200, 200)
+    DARKGREY = (100, 100, 100)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
 
 class World:
-
-    def __init__(self):
-        self.width = 640
-        self.height = 640
-        self.blockSize = 40
+    def __init__(self, width=640, height=640, blockSize=40):
+        self.width = width
+        self.height = height
+        self.blockSize = blockSize
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill((0, 0, 0))
         
@@ -15,8 +23,7 @@ class World:
         for x in range(0, self.width, self.blockSize):
             for y in range(0, self.height, self.blockSize):
                 rect = pygame.Rect(x, y, self.blockSize, self.blockSize)
-                pygame.draw.rect(self.screen, (100, 100, 100), rect, 1)
-    
+                pygame.draw.rect(self.screen, Color.DARKGREY.value, rect, 1)
     def main(self):
         pygame.init()
         self.drawGrid()
@@ -26,6 +33,7 @@ class World:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()
+
 
 if __name__ == "__main__":
     w = World()
