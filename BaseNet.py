5f2df9cd656213d3be395441eb3b56a0f2d58c99
NonsansWD <nonsans.xlsx@gmail.com>
2024-01-29 02:50:22 +0100

    quick fix for nn module

--- BaseNet.py
+++ BaseNet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy
 
-class BaseNet(nn.module):
+class BaseNet(nn.Module):
     def __init__(self):
         super(BaseNet, self).__init__()
         self.fc1 = nn.Linear(6, 512)
